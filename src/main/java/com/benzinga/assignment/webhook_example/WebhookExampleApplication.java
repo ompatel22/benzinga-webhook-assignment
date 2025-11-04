5d8d0d2e2952be73bb03eedf08046101d423573a
ompatel22 <omupatel22@gmail.com>
2025-11-05 01:22:11 +0530

    initial Spring Boot project setup for webhook example

---
+++ src/main/java/com/benzinga/assignment/webhook_example/WebhookExampleApplication.java
@@ -0,0 +1,13 @@
+package com.benzinga.assignment.webhook_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benzinga/assignment/webhook_example/WebhookExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benzinga.assignment.webhook_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
